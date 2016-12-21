967756ca941276e165eb22083d201b86f698a60c
Taras Beyda <tarasbeyda@gmail.com>
2016-12-21 20:41:48 +0200

    subscribe

--- app/config.js
+++ app/config.js
@@ -4,10 +4,20 @@ angular.module('InfWebSite')
     $routeProvider
     
         .when('/', {
+            controller: 'mainPageCtrl',
             templateUrl: 'app/templates/mainPage.html',
+            title: 'JOURNAL'
         })
         .otherwise({
             template: '<h1 style="text-align: center">404</h1>'
         })
     
     })
+
+    .run(['$rootScope', function($rootScope) {
+        
+        $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+            $rootScope.title = current.$$route.title;
+        });
+        
+    }])
