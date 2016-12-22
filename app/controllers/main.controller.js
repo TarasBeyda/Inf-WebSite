1fda02a13a97388f339067113297106b3f0e2f91
Taras Beyda <tarasbeyda@gmail.com>
2016-12-22 18:52:57 +0200

    small nav menu active alfa

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -1,11 +1,63 @@
 angular.module('Controllers', [])
 
     .controller('mainPageCtrl', mainPageCtrl)
-    mainPageCtrl.$inject = ['Factory']
+    mainPageCtrl.$inject = ['mainPageFact']
     mainPageCtrl.$inject = ['$http']
 
-function mainPageCtrl(Factory, $http) {
+function mainPageCtrl(mainPageFact, $http) {
 
+    this.showLeftMenu = function() {
+        
+        $('#btnSmallMenuLeft').toggleClass('active');
+        
+        if ($('#btnSmallMenuLeft').hasClass('active')) {
+//            $('.small__menu__left').animate({
+//                display: 'block'
+//            })
+            $('.small__menu__left').css('display', 'block');
+        } else {
+//            $('.small__menu__left').animate({
+//                display: 'none'
+//            })
+            $('.small__menu__left').css('display', 'none');
+        }
+        
+    };
     
+    this.showRightMenu = function() {
+        
+        $('#btnSmallMenuRight').toggleClass('active');
+        
+        if ($('#btnSmallMenuRight').hasClass('active')) {
+//            $('.small__menu__left').animate({
+//                display: 'block'
+//            })
+            $('.small__menu__right').css('display', 'block');
+        } else {
+//            $('.small__menu__left').animate({
+//                display: 'none'
+//            })
+            $('.small__menu__right').css('display', 'none');
+        }
+        
+    };
+    
+    $(document).click(function(e) {
+        
+        var smallMenuLeft = $('.small__menu__left');
+        var btnSmallMenuLeft = $('#btnSmallMenuLeft');   
+        if(e.target!=smallMenuLeft[0]&&!smallMenuLeft.has(e.target).length&&e.target!=btnSmallMenuLeft[0]&&!btnSmallMenuLeft.has(e.target).length&&btnSmallMenuLeft.hasClass('active')) { 
+            $('.small__menu__left').css('display', 'none');
+            btnSmallMenuLeft.removeClass('active');
+        }
+        
+        var smallMenuRight = $('.small__menu__right');
+        var btnSmallMenuRight = $('#btnSmallMenuRight'); 
+        if(e.target!=smallMenuRight[0]&&!smallMenuRight.has(e.target).length&&e.target!=btnSmallMenuRight[0]&&!btnSmallMenuRight.has(e.target).length&&btnSmallMenuRight.hasClass('active')) { 
+            $('.small__menu__right').css('display', 'none');
+            btnSmallMenuRight.removeClass('active');
+        }
+        
+    });
     
 }
