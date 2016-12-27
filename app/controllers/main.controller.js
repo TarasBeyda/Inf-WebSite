2ac5671241d16a0fc5aa6e4a88d532bbed647940
Taisenberg <tarasbeyda@gmail.com>
2016-12-27 21:29:01 +0200

    slider

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -63,12 +63,9 @@ function mainPageCtrl(mainPageFact, $http) {
     this.slider = function(width_li,margin_right_li,margin_left_li,col_view_img) {
         var step=width_li+margin_right_li+100,
             slider_box_with=col_view_img*step-margin_right_li,
-            $col_img=$(".slider_box>nav>div").length,
+            $col_img=$(".slider_box>nav>section").length,
             col_main_left=0,
             max_col_main_left=$col_img*step;
-        $(".slider_box").width(slider_box_with+50);
-        $(".slider_box>nav>div").width(width_li).css("margin-right",margin_right_li);
-        $(".slider_box>nav>div").width(width_li).css("margin-left",margin_left_li);
         $(".left_nav").click(function(){
             if(col_main_left==0){
                 col_main_left=-max_col_main_left+col_view_img*step;
@@ -85,15 +82,23 @@ function mainPageCtrl(mainPageFact, $http) {
             }
             $(".slider_box>nav").css("margin-left",col_main_left+"px");
         });
-        setInterval(function(){
-            if(-col_main_left==max_col_main_left-col_view_img*step) {
-                col_main_left=0;
-            } else {
-                col_main_left=col_main_left-step;
-                $(".slider_box>nav").css("margin-left",col_main_left+"px");
-            }
-        },4000);
+//        setInterval(function(){
+//            if(-col_main_left==max_col_main_left-col_view_img*step) {
+//                col_main_left=0;
+//                $(".slider_box>nav").css("margin-left",col_main_left+"px");
+//            } else {
+//                col_main_left=col_main_left-step;
+//                $(".slider_box>nav").css("margin-left",col_main_left+"px");
+//            }
+//        },6000);
     };
-    this.slider(300,100,100,1);
+    var browserMaxWidth = parseInt($('.wrapper').css('max-width'), 10);
+    if (browserMaxWidth === 767) {
+        this.slider(400,100,100,1);
+    } else if (browserMaxWidth === 991) {
+        this.slider(500,100,100,1);
+    } else if (browserMaxWidth === 10000) {
+        this.slider(700,100,100,1);
+    };
     
 }
