0ea02c04b027a820b48310099c294cf41fd3b64e
Taisenberg <tarasbeyda@gmail.com>
2016-12-28 23:38:27 +0200

    small menu

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -5,61 +5,60 @@ angular.module('Controllers', [])
     mainPageCtrl.$inject = ['$http']
 
 function mainPageCtrl(mainPageFact, $http) {
-
-    this.showLeftMenu = function() {
-        
-        $('#btnSmallMenuLeft').toggleClass('active');
-        
-        if ($('#btnSmallMenuLeft').hasClass('active')) {
-//            $('.small__menu__left').animate({
-//                display: 'block'
-//            })
-            $('.small__menu__left').css('display', 'block');
-        } else {
-//            $('.small__menu__left').animate({
-//                display: 'none'
-//            })
-            $('.small__menu__left').css('display', 'none');
-        }
-        
-    };
     
-    this.showRightMenu = function() {
+    /*Small menu*/
+    this.showSmallMenu = function() {
         
-        $('#btnSmallMenuRight').toggleClass('active');
+        $('#smallMenuBtn').addClass('active');
         
-        if ($('#btnSmallMenuRight').hasClass('active')) {
-//            $('.small__menu__left').animate({
-//                display: 'block'
-//            })
-            $('.small__menu__right').css('display', 'block');
-        } else {
-//            $('.small__menu__left').animate({
-//                display: 'none'
-//            })
-            $('.small__menu__right').css('display', 'none');
-        }
+            if ($('#smallMenuBtn').hasClass('active')) {
+                $('#smallMenuBtn:hover').css('cursor', 'default');
+                $('.small__menu').animate({
+                    height: '200px'
+                }, 1000);
+                $('#smallMenuBtn').animate({
+                    opacity: '0'
+                }, 1000);
+            };
+        
+    };  
+    
+    this.hideSmallMenu = function() {
+        
+        $('#smallMenuBtnBottom').addClass('active');
+        
+        if ($('#smallMenuBtn').hasClass('active')) {
+            $('.small__menu').animate({
+                height: '40px'
+            }, 1000);
+            $('#smallMenuBtn').animate({
+                opacity: '1'
+            }, 1000);
+            $('#smallMenuBtn').removeAttr('style');
+            $('#smallMenuBtn').removeClass('active');
+        };
         
     };
     
     $(document).click(function(e) {
-        
-        var smallMenuLeft = $('.small__menu__left');
-        var btnSmallMenuLeft = $('#btnSmallMenuLeft');   
-        if(e.target!=smallMenuLeft[0]&&!smallMenuLeft.has(e.target).length&&e.target!=btnSmallMenuLeft[0]&&!btnSmallMenuLeft.has(e.target).length&&btnSmallMenuLeft.hasClass('active')) { 
-            $('.small__menu__left').css('display', 'none');
-            btnSmallMenuLeft.removeClass('active');
+                
+        var smallMenu = $('.small__menu');
+        var smallMenuBtn = $('#smallMenuBtn');   
+        if(e.target!=smallMenu[0]&&!smallMenu.has(e.target).length&&e.target!=smallMenuBtn[0]&&!smallMenuBtn.has(e.target).length&&smallMenuBtn.hasClass('active')) { 
+            $('.small__menu').animate({
+                height: '40px'
+            }, 1000);
+            $('#smallMenuBtn').animate({
+                opacity: '1'
+            }, 1000);
+            $('#smallMenuBtn').removeAttr('style');
+            smallMenuBtn.removeClass('active');
         }
-        
-        var smallMenuRight = $('.small__menu__right');
-        var btnSmallMenuRight = $('#btnSmallMenuRight'); 
-        if(e.target!=smallMenuRight[0]&&!smallMenuRight.has(e.target).length&&e.target!=btnSmallMenuRight[0]&&!btnSmallMenuRight.has(e.target).length&&btnSmallMenuRight.hasClass('active')) { 
-            $('.small__menu__right').css('display', 'none');
-            btnSmallMenuRight.removeClass('active');
-        }
-        
+                
     });
     
+    
+    /*Slider*/
     this.slider = function(width_li,margin_right_li,margin_left_li,col_view_img) {
         var step=width_li+margin_right_li+100,
             slider_box_with=col_view_img*step-margin_right_li,
